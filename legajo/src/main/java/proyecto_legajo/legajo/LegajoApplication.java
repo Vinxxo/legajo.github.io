37a9bc3db1d4b05a0e31131529af1c0e1f2a09bb
Vinxxo <davidemanuel10288@gmail.com>
2025-11-10 19:16:57 -0500

    agregando frontend

---
+++ legajo/src/main/java/proyecto_legajo/legajo/LegajoApplication.java
@@ -0,0 +1,13 @@
+package proyecto_legajo.legajo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegajoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegajoApplication.class, args);
+	}
+
+}
