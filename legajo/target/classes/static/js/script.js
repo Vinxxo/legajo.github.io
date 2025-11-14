c49b0e5b7805ef6e7a600880a605eff44c297292
Vinxxo <davidemanuel10288@gmail.com>
2025-11-14 17:45:17 -0500

    se creó ventana emergente

--- legajo/target/classes/static/js/script.js
+++ legajo/target/classes/static/js/script.js
@@ -22,46 +22,72 @@ toggle.addEventListener("click", () => {
     GRAFICAS DEL ADMIN
 ======================== */
 
-const uR = document.getElementById('uR').getContext('2d');
+if (document.getElementById('uR')) {
+    const uR = document.getElementById('uR').getContext('2d');
 
-new Chart(uR, {
-    type: 'bar',
-    data: {
-        labels: [1,2,11,20,24,30],
-        datasets: [{
-            label: 'Cantidad de reportados (acumulados)',
-            data: [5, 10, 20, 30, 40, 50],
-            backgroundColor: '#999',
-            borderRadius: 6
-        }]
-    },
-    options: {
-            plugins: { legend: { display: false } },
-            scales: {   
-            y: { beginAtZero: true },
-            x: { grid: { display: false } }
+    new Chart(uR, {
+        type: 'bar',
+        data: {
+            labels: [1,2,11,20,24,30],
+            datasets: [{
+                label: 'Cantidad de reportados (acumulados)',
+                data: [5, 10, 20, 30, 40, 50],
+                backgroundColor: '#999',
+                borderRadius: 6
+            }]
+        },
+        options: {
+                plugins: { legend: { display: false } },
+                scales: {
+                y: { beginAtZero: true },
+                x: { grid: { display: false } }
+                }
             }
-        }
+    });
+}
+
+if (document.getElementById('iC')) {
+    const iC = document.getElementById('iC').getContext('2d');
+
+    new Chart(iC, {
+        type: 'line',
+        data: {
+            labels: [1,2,11,20,24,30],
+            datasets: [{
+                label: 'intercambios confirmados',
+                data: [5, 25, 20, 30, 80, 100],
+                backgroundColor: '#999',
+                borderRadius: 6
+            }]
+        },
+        options: {
+                plugins: { legend: { display: false } },
+                scales: {
+                y: { beginAtZero: true },
+                x: { grid: { display: false } }
+                }
+            }
+        
+    })
+}
+
+/* ======================
+    VENTANA EMERGENTE
+====================== */
+
+const modal = document.getElementById('modal');
+const close = document.getElementById('closeModal');
+
+// Seleccionar todos los botones con clase 'ver-libro'
+const openButtons = document.querySelectorAll('.ver-libro');
+
+// Agregar event listener a cada botón
+openButtons.forEach(button => {
+    button.onclick = () => modal.style.display = 'flex';
 });
 
-const iC = document.getElementById('iC').getContext('2d');
+close.onclick = () => modal.style.display = 'none';
 
-new Chart(iC, {
-    type: 'line',
-    data: {
-        labels: [1,2,11,20,24,30],
-        datasets: [{
-            label: 'intercambios confirmados',
-            data: [5, 25, 20, 30, 80, 100],
-            backgroundColor: '#999',
-            borderRadius: 6
-        }]
-    },
-    options: {
-            plugins: { legend: { display: false } },
-            scales: {   
-            y: { beginAtZero: true },
-            x: { grid: { display: false } }
-            }
-        }
-});
+window.onclick = (e) => {
+    if (e.target === modal) modal.style.display = 'none';
+};
